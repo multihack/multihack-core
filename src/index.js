081737a4b76ebe4bdb8f875827453bc9dba364b0
RationalCoding <thomasmullendesign@gmail.com>
2017-06-21 13:51:32 -0400

    wait for yfs to be ready

--- src/index.js
+++ src/index.js
@@ -49,6 +49,16 @@ function RemoteManager (opts) {
     }
   }
   
+  self.onceReady = function (f) {
+    if (!self.yfs) {
+      self.once('ready', function () {
+        f()
+      })
+    } else {
+      f()
+    }
+  }
+  
   Y({
     db: {
       name: 'memory' // Store the CRDT model in memory
@@ -149,14 +159,18 @@ RemoteManager.prototype.getContent = function (filePath) {
 RemoteManager.prototype.createFile = function (filePath, contents) {
   var self = this
   
-  self.yfs.set(filePath, Y.Text)
-  insertChunked(self.yfs.get(filePath), 0, contents || '')
+  self.onceReady(function () {
+    self.yfs.set(filePath, Y.Text)
+    insertChunked(self.yfs.get(filePath), 0, contents || '')
+  })
 }
 
 RemoteManager.prototype.createDir = function (filePath, contents) {
   var self = this
   
-  self.yfs.set(filePath, 'DIR')
+  self.onceReady(function () {
+    self.yfs.set(filePath, 'DIR')
+  })
 }
 
 function insertChunked(ytext, start, str) {
@@ -182,57 +196,68 @@ function chunkString(str, size) {
 RemoteManager.prototype.replaceFile = function (oldPath, newPath) {
   var self = this
   
-  self.yfs.set(newPath, self.yfs.get(oldPath))
+  self.onceReady(function () {
+    self.yfs.set(newPath, self.yfs.get(oldPath))
+  })
 }
   
 RemoteManager.prototype.deleteFile = function (filePath) {
   var self = this
   
-  self.yfs.delete(filePath)
+  self.onceReady(function () {
+    self.yfs.delete(filePath)
+  })
 }
 
 RemoteManager.prototype.changeFile = function (filePath, delta) {
   var self = this
   
-  self.mutualExcluse(filePath, function () {
-    var ytext = self.yfs.get(filePath)
-    if (!ytext) self.createFile(filePath, '')
-
-    // apply the delta to the ytext instance
-    var start = delta.start
-
-    // apply the delete operation first
-    if (delta.removed.length > 0) {
-      var delLength = 0
-      for (var j = 0; j < delta.removed.length; j++) {
-        delLength += delta.removed[j].length
+  self.onceReady(function () {
+    self.mutualExcluse(filePath, function () {
+      var ytext = self.yfs.get(filePath)
+      if (!ytext) {
+        self.createFile(filePath, '')
+        ytext = self.yfs.get(filePath)
       }
-      // "enter" is also a character in our case
-      delLength += delta.removed.length - 1
-      ytext.delete(start, delLength)
-    }
 
-    // apply insert operation
-    insertChunked(ytext, start, delta.text.join('\n'))
+      // apply the delta to the ytext instance
+      var start = delta.start
+
+      // apply the delete operation first
+      if (delta.removed.length > 0) {
+        var delLength = 0
+        for (var j = 0; j < delta.removed.length; j++) {
+          delLength += delta.removed[j].length
+        }
+        // "enter" is also a character in our case
+        delLength += delta.removed.length - 1
+        ytext.delete(start, delLength)
+      }
+
+      // apply insert operation
+      insertChunked(ytext, start, delta.text.join('\n'))
+    })
   })
 }
 
 RemoteManager.prototype.changeSelection = function (data) {
   var self = this
   
-  // remove our last select first
-  if (self.lastSelection !== null) {
-    self.ySelections.toArray().forEach(function (a, i) {
-      if (a.tracker === self.lastSelection) {
-        self.ySelections.delete(i)
-      }
-    })
-  } 
-  
-  data.id = self.id
-  data.tracker = Math.random()
-  self.lastSelection = data.tracker
-  self.ySelections.push([data])
+  self.onceReady(function () {
+    // remove our last select first
+    if (self.lastSelection !== null) {
+      self.ySelections.toArray().forEach(function (a, i) {
+        if (a.tracker === self.lastSelection) {
+          self.ySelections.delete(i)
+        }
+      })
+    } 
+
+    data.id = self.id
+    data.tracker = Math.random()
+    self.lastSelection = data.tracker
+    self.ySelections.push([data])
+  })
 }
 
 RemoteManager.prototype._onYTextAdd = function (filePath, event) {
